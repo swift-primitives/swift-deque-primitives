790024b6b23bcef105d4c60411b74bcbc3ccb9c9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:23:21 +0100

    Add Collection Primitives dependency
    Deques are collections and should depend on Collection Primitives for
    collection view types and transformations.

--- Package.swift
+++ Package.swift
@@ -17,9 +17,19 @@ let package = Package(
             targets: ["Deque Primitives"]
         )
     ],
+    dependencies: [
+        .package(path: "../swift-index-primitives"),
+        .package(path: "../swift-input-primitives"),
+        .package(path: "../swift-collection-primitives"),
+    ],
     targets: [
         .target(
-            name: "Deque Primitives"
+            name: "Deque Primitives",
+            dependencies: [
+                .product(name: "Index Primitives", package: "swift-index-primitives"),
+                .product(name: "Input Primitives", package: "swift-input-primitives"),
+                .product(name: "Collection Primitives", package: "swift-collection-primitives"),
+            ]
         ),
         .testTarget(
             name: "Deque Primitives Tests",
